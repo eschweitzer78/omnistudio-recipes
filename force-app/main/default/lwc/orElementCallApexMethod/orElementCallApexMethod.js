450c2f7efcccfc61102a15426ae46218bbd1ad79
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2022-08-26 14:29:19 +1000

    update with promise

--- force-app/main/default/lwc/orElementCallApexMethod/orElementCallApexMethod.js
+++ force-app/main/default/lwc/orElementCallApexMethod/orElementCallApexMethod.js
@@ -5,9 +5,8 @@ import OmniscriptRestApi from "omnistudio/omniscriptRestApi";
 import tmpl from "./orElementCallApexMethod.html";
 
 /*
-  Please refer to the disclaimer in README.rd at the root of this repo.
-  This recipe is about calling Apex from an Omniscript element.
-  The target Apex class should be implementing Callback as illustrated in orHelloWord.cls
+   This recipe is about calling Apex from an Omniscript element.
+   The target Apex class should be implementing Callback as illustrated in orHelloWord.cls
 */
 
 export default class OmniElementCallApexMethod extends OmniscriptBaseElement {
@@ -16,21 +15,24 @@ export default class OmniElementCallApexMethod extends OmniscriptBaseElement {
   }
 
   handleClick() {
-    try {
-      let r = invokeApexMethod(OmniscriptRestApi.GenericInvoke2NoCont, {
-        input: JSON.stringify({
-          name: "Astro"
-        }),
-        options: JSON.stringify({
-        }),
-        sClassName: "orHelloWorld",
-        sMethodName: "greet"
-      });
-
-      console.log('r=', JSON.stringify(r));
-    } catch (err) {
-      console.log('Exception', err);
-    }
+    invokeApexMethod(OmniscriptRestApi.GenericInvoke2NoCont, {
+      input: JSON.stringify({
+        name: "Astro"
+      }),
+      options: JSON.stringify({
+      }),
+      sClassName: "orHelloWorld",
+      sMethodName: "greet"
+    })
+    .then(data => {
+      // Replace with your own response handling
+      let jsonData = JSON.parse(data);
+      console.log('response', jsonData);
+    })
+    .catch(err => {
+      // Replace with your own error handling
+      console.log('error', err);
+    });
   }
 
   @api checkValidity() {
