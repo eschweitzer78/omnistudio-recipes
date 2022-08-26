a7416c1e9dba08d2387528ed756e3b50249b76cb
Emmanuel Schweitzer <eschweitzer78@yahoo.com>
2022-08-26 14:26:42 +1000

    update re. disclaimer

--- force-app/main/default/lwc/orElementCallApexMethod/orElementCallApexMethod.js
+++ force-app/main/default/lwc/orElementCallApexMethod/orElementCallApexMethod.js
@@ -5,8 +5,9 @@ import OmniscriptRestApi from "omnistudio/omniscriptRestApi";
 import tmpl from "./orElementCallApexMethod.html";
 
 /*
-   This recipe is about calling Apex from an Omniscript element.
-   The target Apex class should be implementing Callback as illustrated in orHelloWord.cls
+  Please refer to the disclaimer in README.rd at the root of this repo.
+  This recipe is about calling Apex from an Omniscript element.
+  The target Apex class should be implementing Callback as illustrated in orHelloWord.cls
 */
 
 export default class OmniElementCallApexMethod extends OmniscriptBaseElement {
